60bcbd404df2a161931715af6bac87ca9d383966
igranet <igranet@wustl.edu>
2023-01-04 16:06:41 -0800

    asdf

--- src/components/Edge3.js
+++ src/components/Edge3.js
@@ -4,7 +4,7 @@ const EDGE3 = ()=>{
     const [hover3, setHover3] = useState(false)
     return (
         <div onMouseEnter={()=>{setHover3(true)}} onMouseOut={()=>{setHover3(false)}} className={`cursor-pointer`}>
-            <span className={`cursor-pointer ${hover3 && " rounded-sm"} duration-500 px-2 py-1 transition-all w-full`}>edge</span><span className={`${hover3 && " text-red-400"} duration-400 transition-all`}>3</span>
+            <span className={`cursor-pointer ${hover3 && " rounded-sm "} duration-500 px-2 py-1 transition-all w-full`}>edge</span><span className={`${hover3 && " text-red-400 "} duration-500 px-2 py-1 transition-all`}>3</span>
         </div>
 
     )
